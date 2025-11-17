266c1020d2d2c4f05f8223f47718562dd2b601b7
madeval <alvarez.fing@gmail.com>
2025-11-17 18:56:08 -0300

    Refactor apply button event listener to use class-based targeting
    The event listener is updated to target `.jobs-listings` instead of `.job-list-component` and uses `classList.contains('btn-std')` instead of `nodeName` check for better specificity and error prevention.

--- 02-ejercicio-javascript/apply-button.js
+++ 02-ejercicio-javascript/apply-button.js
@@ -1,8 +1,11 @@
-const jobList = document.querySelector('.job-list-component');
+// buscamos por la lista de empleos, no todo el section. Esto nos permite limitar la búsqueda y evitar errores por si algo más fuera del section tuviera la misma clase
+const jobList = document.querySelector('.jobs-listings');
 
 jobList.addEventListener('click', function(e){
-    if (e.target.nodeName == 'BUTTON'){
-        const button = e.target;
+    // no es buena práctica usar nodeName, es mejor usar className para limitar la búsqueda y evitar errores
+    const button = e.target;
+    
+    if (button.classList.contains('btn-std')){
         button.textContent = '!Aplicado!';
         button.classList.add('btn-applied');
         button.disabled = true;
