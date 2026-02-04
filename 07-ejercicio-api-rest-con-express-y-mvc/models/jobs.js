03af340608fd369954069b463964c8f5267c124c
Visto y no Victor <vistoynovictor@gmail.com>
2026-02-04 21:03:36 +0100

    Add 07-ejercicio-api-rest-con-express-y-mvc/models/jobs.js

---
+++ 07-ejercicio-api-rest-con-express-y-mvc/models/jobs.js
@@ -0,0 +1,4 @@
+import jobs from '../jobs.json' with { type: 'json' }
+
+/* Aquí deberá ir la lógica de tu modelo */
+/* Recuerda que el modelo SOLO debe manejar la lógica de los datos, en este caso nuestro JSON */
