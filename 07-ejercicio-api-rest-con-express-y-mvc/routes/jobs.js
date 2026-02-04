4c08cfa587716b4f7df0473ca58e9d43e9aa5076
Visto y no Victor <vistoynovictor@gmail.com>
2026-02-04 21:03:38 +0100

    Add 07-ejercicio-api-rest-con-express-y-mvc/routes/jobs.js

---
+++ 07-ejercicio-api-rest-con-express-y-mvc/routes/jobs.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+
+export const jobsRouter = Router()
+
+/* Aquí debe ir la lógica de tus rutas */
+/* Recuerda que en tus rutas debes usar los controladores */
+/* 
+Deberás implementar:
+- Obtener todos los jobs [GET]
+- Obtener un job por id [GET]
+- Crear un job [POST]
+- Actualizar un job por id [PUT]
+- Actualizar parcialmente un job por id [PATCH]
+- Eliminar un job por id [DELETE]
+*/
