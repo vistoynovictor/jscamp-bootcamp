b0aa5981738f3ddafc34db07418df17ed05f5355
Visto y no Victor <vistoynovictor@gmail.com>
2026-02-06 15:02:10 +0100

    Add app implementation

--- 07-ejercicio-api-rest-con-express-y-mvc/app.js
+++ 07-ejercicio-api-rest-con-express-y-mvc/app.js
@@ -1,11 +1,22 @@
 import express from 'express'
+import { corsMiddleware } from './middlewares/cors.js'
 import { jobsRouter } from './routes/jobs.js'
 
-const PORT = 3000
+import { DEFAULTS } from './config.js'
+
+const PORT = process.env.PORT ?? DEFAULTS.PORT
+
 const app = express()
+
+app.use(corsMiddleware())
+app.use(express.json())
 
 app.use('/jobs', jobsRouter)
 
-app.listen(PORT, () => {
-  console.log(`Servidor levantado en http://localhost:${PORT}`)
-})
+if (!process.env.NODE_ENV){
+    app.listen(PORT, () => {
+        console.log(`Servidor escuchando en http://localhost:${PORT}`)
+    })
+}
+
+export default app
