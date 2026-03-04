e78550bd1796d3d7240d58e35ccef13d8dcb8422
Visto y no Victor <vistoynovictor@gmail.com>
2026-03-04 13:23:07 +0100

    Add 08-ejercicio-testing-con-node-y-zod/app.js

---
+++ 08-ejercicio-testing-con-node-y-zod/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { corsMiddleware } from './middlewares/cors.js'
+import { jobsRouter } from './routes/jobs.js'
+
+import { DEFAULTS } from './config.js'
+
+const PORT = process.env.PORT ?? DEFAULTS.PORT
+
+const app = express()
+
+app.use(corsMiddleware())
+app.use(express.json())
+
+app.use('/jobs', jobsRouter)
+
+if (!process.env.NODE_ENV) {
+  app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
+  })
+}
+
+export default app
