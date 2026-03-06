f8dcf73900d186ad1d1ea23e9b5644cef5225e5a
Visto y no Victor <vistoynovictor@gmail.com>
2026-03-06 12:44:27 +0100

    Add 10-ejercicio-typescript/types.ts

---
+++ 10-ejercicio-typescript/types.ts
@@ -0,0 +1,8 @@
+/* Aquí deberás definir los tipos literales del primer ejercicio */
+
+/* 
+export type ExperienceLevel = 
+export type WorkMode = 
+export type ApplicationStatus = 
+export type Technology = 
+*/
