d1205c6d13822e680b38bcae60042423c06b4a11
Visto y no Victor <vistoynovictor@gmail.com>
2026-03-06 12:44:24 +0100

    Add 10-ejercicio-typescript/objects.ts

---
+++ 10-ejercicio-typescript/objects.ts
@@ -0,0 +1,9 @@
+/* Aquí deberás definir los tipos para los objetos Job, Company y Candidate */
+
+/* 
+import type { ExperienceLevel, WorkMode, Technology } from './types.ts'
+
+export type Job = 
+export type Company = 
+export type Candidate = 
+*/
